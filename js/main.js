122258ba650bae73a07aeaf178a7105973c33904
WesleySOAD <166054167+WesleySOAD@users.noreply.github.com>
2024-06-17 20:55:50 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Variable Declaration
+
+    const loginBtn = document.querySelector("#login");
+    const registerBtn = document.querySelector("#register");
+    const loginForm = document.querySelector(".login-form");
+    const registerForm = document.querySelector(".register-form");
+
+// Login button function
+loginBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "#00a0fd33";
+    registerBtn.style.backgroundColor = "rgba(255, 255, 255 0.2)";
+
+    loginForm.style.left ="50%";
+    registerForm.style.left ="-50%";
+    
+    loginForm.style.opacity = 1;
+    registerForm.style.opacity = 0;
+
+    document.querySelector(".col-1").style.borderRadius ="0 30% 20% 0";
+})
+
+
+// Register button functionloginBtn.addEventListener('click', () => {
+    registerBtn.addEventListener('click', () => {
+        loginBtn.style.backgroundColor = "#00a0fd33";
+        registerBtn.style.backgroundColor = "rgba(255, 255, 255 0.2)";
+    
+        loginForm.style.left ="150%";
+        registerForm.style.left ="50%";
+
+        loginForm.style.opacity = 0;
+        registerForm.style.opacity = 1;
+
+        document.querySelector(".col-1").style.borderRadius ="0 20% 30% 0";
+    })
